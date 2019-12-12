b583c3092db6b80814f154d0a96d55a46921013b
Damyan Peykov <dppeykov@gmail.com>
2019-12-12 11:47:22 +0100

    Adding test recipes
    End of video 53 - section 3 - adding testing recipes and updating part of the styles

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: "app-recipe-list",
@@ -6,7 +7,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test",
+      "Simply a test",
+      "https://deliciouslyella.com/wp-content/uploads/2019/07/speedy-tomato-pasta-recipe-2.jpg"
+    ),
+    new Recipe(
+      "Test1",
+      "Simply a test1",
+      "https://deliciouslyella.com/wp-content/uploads/2019/07/speedy-tomato-pasta-recipe-2.jpg"
+    ),
+    new Recipe(
+      "Test2",
+      "Simply a test2",
+      "https://deliciouslyella.com/wp-content/uploads/2019/07/speedy-tomato-pasta-recipe-2.jpg"
+    )
+  ];
 
   constructor() {}
 
